c9bdd103a5d5b7e7e55c56faf499e8dc3df746eb
Siro_256 <siro@siro256.dev>
2022-03-13 07:38:55 +0900

    Add Minecraft to dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,16 @@
+plugins {
+    //Latest version: https://fabricmc.net/develop/
+    id("fabric-loom") version "0.11-SNAPSHOT"
+}
+
 group = rootProject.group.toString() + ".core"
+
+dependencies {
+    //Minecraft API, using fabric's mapping
+    //Latest version: https://fabricmc.net/develop/
+    add("minecraft", "com.mojang:minecraft:1.18.2")
+    add("mappings", loom.officialMojangMappings())
+}
 
 tasks {
     getByName("jar", Jar::class) {
