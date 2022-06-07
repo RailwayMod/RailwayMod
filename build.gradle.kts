f3004f16b40f0292bbbb4ba73a80d41e60bf4a68
Siro_256 <siro@siro256.dev>
2022-06-08 02:47:27 +0900

    Remove process of including license from ProcessResources

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,6 @@ allprojects {
 
         withType<ProcessResources> {
             duplicatesStrategy = DuplicatesStrategy.INCLUDE
-
-            from(rootProject.file("LICENSE").path)
-            rename { "LICENSE_${rootProject.name}" }
         }
 
         withType<Jar> {
